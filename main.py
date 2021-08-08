1adfe2addbbb66c583d656a3618a98ed47028675
Giovanni Prinzivalli <Gprinziv@gmail.com>
2021-08-08 05:12:23 +0000

    Cleaning pass. Made it more extensible to passwords of any length (min 6). Made some of the logic more readable, too.

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
-def findpass(password):
+import time
+
+def passgen(password):
   newpass = [ord(char) for char in oldpass]
 
   ptr = 0
   while ptr < len(newpass):
-    if newpass[ptr] in (ord("i"), ord("l"), ord("o")):
+    if chr(newpass[ptr]) in "ilo":
       newpass[ptr] += 1
-      ptr += 1
-      while ptr < len(newpass):
-        newpass[ptr] = ord("a")
-        ptr += 1
+      newpass = newpass[:ptr + 1] + [97] * (len(newpass) - ptr - 1)
       break
     ptr += 1
+    
 
-  if newpass[3] < newpass[4]:
-    newpass[3] += 1
-  elif newpass[3] == newpass[4]:
-    if (newpass[4] + 1) < newpass[5]:
-      newpass[3] += 1
-    elif (newpass[4] + 1) == newpass[5]:
-      if (newpass[5] + 1) < newpass[6]:
-        newpass[3] += 1
-      elif (newpass[5] + 1) == newpass[6]:
-        if newpass[6] <= newpass[7]:
-          newpass[3] += 1
-  if chr(newpass[3]) in "ghijklmno":
+  if newpass[-5] < newpass[-4]:
+    newpass[-5] += 1
+  elif newpass[-5] == newpass[-4]:
+    if (newpass[-4] + 1) < newpass[-3]:
+      newpass[-5] += 1
+    elif (newpass[-4] + 1) == newpass[-3]:
+      if (newpass[-3] + 1) < newpass[-2]:
+        newpass[-5] += 1
+      elif (newpass[-3] + 1) == newpass[-2]:
+        if newpass[-2] <= newpass[-1]:
+          newpass[-5] += 1
+  if chr(newpass[-5]) in "ghijklmno":
     newpass = newpass[:3] + [97, 97, 98, 99, 99]
-  elif chr(newpass[3]) in "yz":
-    newpass[2] += 1
-    newpass = newpass[:3] + [97, 97, 98, 99, 99]
+  elif chr(newpass[-5]) in "yz":
+    newpass[-6] += 1
+    newpass = newpass[:-5] + [97, 97, 98, 99, 99]
 
-  newpass[4] = newpass[3]
-  newpass[5] = newpass[3] + 1
-  newpass[6] = newpass[3] + 2
-  newpass[7] = newpass[3] + 2
+  newpass = newpass[:-4] + [newpass[-5], newpass[-5] + 1, newpass[-5] + 2, newpass[-5] + 2]
   print(newpass)
   
   return "".join((chr(number) for number in newpass))  
@@ -45,6 +42,9 @@ def findpass(password):
     #If one match, optimal tail configuration is ######11  
 
 oldpass = "cqjxjnds"
-newpass = findpass(oldpass)
+starttime = time.time()
+newpass = passgen(oldpass)
+endtime = time.time()
 
 print(newpass)
+print(endtime - starttime)
